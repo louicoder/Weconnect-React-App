b083600283e6d348aa850f30a4ed75a19514013e
snappy45 <musanje2010@gmail.com>
2018-07-08 17:16:56 +0300

    delete unnecessary files, add index test, modift login and profile tests

--- src/tests/user/Profile.test.js
+++ src/tests/user/Profile.test.js
@@ -10,24 +10,7 @@ describe(<Profile />, () => {
     wrapper = shallow(<MemoryRouter><Profile /></MemoryRouter>);
     component = wrapper.find(Profile).dive();
 
-    beforeEach(() => {
-        
-        // axios.post.mockImplementationOnce(
-        //     jest.fn(() => Promise.resolve({
-        //         data:{
-        //             message:'password successfully changed'
-        //         }
-        //     }))
-        // )
-    });
-    afterEach(() => {
-
-    })
-
     it('renders proper elements on the page', ()=> {
-        // const notif = notification("success", "password successfully changed")
-
-        // localStorage.setItem('token','eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoibG91aXNhIiwiZXhwIjoxNTMxMDgzMDA3LCJpZCI6MTIsInVzZXJuYW1lIjoibG91aXNhIn0.CVZmcaH-6SkqE7bSB6LwD2QWPCmzCyTOdqSCs_PZk90')
         let spy = jest.spyOn(component.instance(), 'resetPassword')
         
         component.find('input[name="password"]').simulate('change', {target: {value:'password'}})
@@ -35,12 +18,8 @@ describe(<Profile />, () => {
         const submit = component.find('button[type="submit"]').simulate('click', {preventDefault: (jest.fn())});
         
         expect(spy).toHaveBeenCalled();
-        // console.log(wrapper)
-        // expect(component.find('div').length).toBe(9)
-        // expect(component.find('form').length).toBe(1)
-        // expect(component.find('input').length).toBe(2)
-        // expect(component.find('div').length).toBe(9)
 
     });
+   
 
 })
