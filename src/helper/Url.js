933b3765b1313c7ce6273a50118f5b51e0142209
snappy45 <musanje2010@gmail.com>
2018-07-12 09:52:18 +0300

    add eslint, configure url

--- src/helper/Url.js
+++ src/helper/Url.js
@@ -1,4 +1,5 @@
+//Url configuration for localhost
 export const BASE_URL = 'http://127.0.0.1:5000/'
-// export default class Config{
-//     static BASE_URL = 'http://127.0.0.1:5000/'
-// }
+
+//Url configuration for heroku hosted application
+// export const BASE_URL = 'http://127.0.0.1:5000/'
