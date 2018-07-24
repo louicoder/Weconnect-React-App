4c527dfc25daffe4880a5ea13401f498171aa22a
snappy45 <musanje2010@gmail.com>
2018-07-24 09:45:50 +0300

    change url to point to heroku [Fixess #159220947]

--- src/helper/Url.js
+++ src/helper/Url.js
@@ -1,6 +1,6 @@
 //Url configuration for localhost
-export const BASE_URL = 'http://127.0.0.1:5000/'
+// export const BASE_URL = 'http://127.0.0.1:5000/'
 
 //Url configuration for heroku hosted application
-// export const BASE_URL = 'https://weconnect-api-app.herokuapp.com/'
+export const BASE_URL = 'https://weconnect-react-app.herokuapp.com/'
  
